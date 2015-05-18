7af7e3f5dfa0766c63f318e83fad9213df486682
xjchengo <xchenxujian@126.com>
2015-05-18 21:57:11 +0800

    add x to all in one

--- all_in_one.sh
+++ all_in_one.sh
@@ -28,44 +28,48 @@ apt-get install -y software-properties-common
 # trying to install PPA behind firewall fails:
 
 # apt-add-repository ppa:nginx/stable -y
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C300EE8C
+# apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C300EE8C
+apt-key add /root/alauda/nginx.key
 # apt-add-repository ppa:rwky/redis -y
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 5862E31D
+# apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 5862E31D
+apt-key add /root/alauda/redis.key
 # apt-add-repository ppa:chris-lea/node.js -y
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C7917B12
+# apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C7917B12
+apt-key add /root/alauda/nodejs.key
 # apt-add-repository ppa:ondrej/php5-5.6 -y
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E5267A6C
+# apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E5267A6C
+apt-key add /root/alauda/php.key
 
 
-# # Setup Postgres 9.4 Repositories
+# Setup Postgres 9.4 Repositories
 
-# wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
-# sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" >> /etc/apt/sources.list.d/postgresql.list'
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" >> /etc/apt/sources.list.d/postgresql.list'
 
-# # Update Package Lists
+# Update Package Lists
 
-# apt-get update
+apt-get update
 
-# # Base Packages
+# Base Packages
 
-# apt-get install -y build-essential curl gcc git libmcrypt4 libpcre3-dev \
-# make python-pip supervisor unzip whois vim
+apt-get install -y build-essential curl gcc git libmcrypt4 libpcre3-dev \
+make python-pip supervisor unzip whois vim
 
-# # Install Python Httpie
+# Install Python Httpie
 
-# pip install httpie
+pip install httpie
 
-# # Set The Timezone
+# Set The Timezone
 
-# ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
 
-# # Install Base PHP Packages
+# Install Base PHP Packages
 
-# apt-get install -y php5-cli php5-dev php-pear \
-# php5-mysqlnd php5-pgsql php5-sqlite \
-# php5-apcu php5-json php5-curl php5-dev php5-gd \
-# php5-gmp php5-imap php5-mcrypt php5-memcached php5-xdebug
+apt-get install -y php5-cli php5-dev php-pear \
+php5-mysqlnd php5-pgsql php5-sqlite \
+php5-apcu php5-json php5-curl php5-dev php5-gd \
+php5-gmp php5-imap php5-mcrypt php5-memcached php5-xdebug
 
 # # Make The MCrypt Extension Available
 
